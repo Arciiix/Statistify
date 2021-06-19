f44b3db9b5163bde6ca2263c23646d9f99280e66
Arciiix <artur.nowak100@gmail.com>
2021-06-19 19:23:26 +0200

    Create a log function (server)

--- server/server.ts
+++ server/server.ts
@@ -5,8 +5,23 @@ import constants from "./secret";
 
 const app = express();
 
+function log(message: string, isError?: boolean): void {
+  let nowDate: Date = new Date();
+
+  //Format the date into HH:mm:ss dd.MM.yyyy
+  let nowString = `${addZero(nowDate.getHours())}:${addZero(
+    nowDate.getMinutes()
+  )}:${addZero(nowDate.getSeconds())} ${addZero(nowDate.getDate())}.${addZero(
+    nowDate.getMonth() + 1
+  )}.${nowDate.getFullYear()}`;
+
+  console.log(`[${isError ? "ERROR" : "LOG"}] [${nowString}] ${message}`);
+}
+
+function addZero(value: number): string {
+  return value < 10 ? `0${value}` : `${value}`;
+}
+
 app.listen(PORT, (): void => {
-  //DEV
-  //TODO: Log
-  console.log(`App has started on the port ${PORT}`);
+  log(`App has started on the port ${PORT}`);
 });
